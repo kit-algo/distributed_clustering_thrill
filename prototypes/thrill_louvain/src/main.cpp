01b5625fdfb5a3ba73be0501b3a7620a3ed221d1
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2016-12-19 14:50:32 +0100

    compiling louvain with working dummy clustering

--- prototypes/thrill_louvain/src/main.cpp
+++ prototypes/thrill_louvain/src/main.cpp
@@ -46,6 +46,10 @@ std::ostream& operator << (std::ostream& os, Edge& e) {
   return os << e.tail << " -> " << e.head;
 }
 
+std::ostream& operator << (std::ostream& os, NodeInfo& node_info) {
+  return os << node_info.id << ": " << node_info.data;
+}
+
 // template<class NodeType>
 // typename NodeType::EdgeType constructEdge(uint32_t, typename NodeType::NeighborType&);
 
@@ -59,6 +63,8 @@ std::ostream& operator << (std::ostream& os, Edge& e) {
 
 template<class EdgeType>
 thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
+  std::cout << "louvain\n";
+
   auto nodes = edge_list
     .Keep()
     .Map([](const EdgeType & edge) { return Node { edge.tail, 1 }; })
@@ -82,9 +88,9 @@ thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
   // Local Moving
   auto node_clusters = edge_list
     .Keep()
-    .Zip(edge_partitions, [](const EdgeType & edge, const uint32_t & partition) { return std::make_pair(partition, edge); })
+    .Zip(thrill::api::NoRebalanceTag, edge_partitions, [](const EdgeType & edge, const uint32_t & partition) { return std::make_pair(partition, edge); })
     // .template GroupByKey<std::vector<std::pair<uint32_t, uint32_t>>>(
-    //   [](const std::pair<uint32_t, Edge> & edge_with_partition) { return edge_with_partition.first; },
+    //   [](const std::pair<uint32_t, EdgeType> & edge_with_partition) { return edge_with_partition.first; },
     //   [](auto &, const uint32_t &) {
     //     // std::vector<Node> nodes;
     //     // std::vector<NodeCluster> mapping;
@@ -101,7 +107,7 @@ thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
     //     return foo;
     //   })
     // .template FlatMap<NodeInfo>(
-    //   [](std::vector<std::pair<uint32_t, uint32_t>> & mapping, const auto emit) {
+    //   [](std::vector<std::pair<uint32_t, uint32_t>> mapping, auto emit) {
     //     for (const std::pair<uint32_t, uint32_t>& pair : mapping) {
     //       emit(NodeInfo { pair.first, pair.second });
     //     }
@@ -118,11 +124,11 @@ thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
 
   // TODO cleanup cluster ids
 
-  bool changed = node_clusters
+  bool unchanged = node_clusters
     .Keep()
     .Map([](const NodeInfo & node_cluster) { return node_cluster.id == node_cluster.data; })
     .AllReduce([](bool acc, const bool value) { return acc && value; }, true);
-  if (!changed) {
+  if (unchanged) {
     return node_clusters;
   }
 
@@ -133,7 +139,7 @@ thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
   auto cluster_id_times_degree = nodes
     .Keep()
     // No rebalance
-    .Zip(clusters, [](const Node & node, const uint32_t cluster_id) { return std::make_pair(node.degree, cluster_id); })
+    .Zip(thrill::api::NoRebalanceTag, clusters, [](const Node & node, const uint32_t cluster_id) { return std::make_pair(node.degree, cluster_id); })
     .template FlatMap<uint32_t>(
       [](std::pair<size_t, uint32_t> degree_and_cluster, auto emit) {
         for (uint32_t i = 0; i < degree_and_cluster.first; i++) {
@@ -144,7 +150,7 @@ thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
   // Build Meta Graph
   auto meta_graph_edges = edge_list
     // Translate Ids
-    .Zip(cluster_id_times_degree.Keep(),
+    .Zip(thrill::api::NoRebalanceTag, cluster_id_times_degree.Keep(),
       [](EdgeType edge, uint32_t new_id) {
         edge.tail = new_id;
         return edge;
@@ -160,7 +166,7 @@ thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
       [](const EdgeType & e1, const EdgeType & e2) {
         return (e1.tail == e2.tail && e1.head < e2.head) || (e1.tail < e2.tail);
       })
-    .Zip(cluster_id_times_degree,
+    .Zip(thrill::api::NoRebalanceTag, cluster_id_times_degree,
       [](EdgeType edge, uint32_t new_id) {
         edge.tail = new_id;
         return edge;
@@ -186,7 +192,7 @@ thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
     .Sort([](const std::pair<uint32_t, uint32_t> & cluster_sizes1, const std::pair<uint32_t, uint32_t> & cluster_sizes2) { return cluster_sizes1.first < cluster_sizes2.first; });
 
   auto new_cluster_ids_times_size = meta_clustering
-    .Zip(cluster_sizes, [](const NodeInfo & meta_node_cluster, const std::pair<uint32_t, uint32_t> & cluster_size) { return std::make_pair(meta_node_cluster.data, cluster_size.second); }) // assert(meta_node_cluster.id == cluster_size.first)
+    .Zip(thrill::api::NoRebalanceTag, cluster_sizes, [](const NodeInfo & meta_node_cluster, const std::pair<uint32_t, uint32_t> & cluster_size) { return std::make_pair(meta_node_cluster.data, cluster_size.second); }) // assert(meta_node_cluster.id == cluster_size.first)
     .template FlatMap<uint32_t>(
       [](const std::pair<uint32_t, uint32_t> & cluster_size, auto emit) {
         for (uint32_t i = 0; i < cluster_size.second; i++) {
@@ -199,7 +205,7 @@ thrill::DIA<NodeInfo> louvain(thrill::DIA<EdgeType>& edge_list) {
       [](const NodeInfo & node_cluster1, const NodeInfo & node_cluster2) {
         return node_cluster1.data < node_cluster2.data;
       })
-    .Zip(new_cluster_ids_times_size, [](const NodeInfo & node_cluster, uint32_t new_cluster_id) { return NodeInfo { node_cluster.id, new_cluster_id }; })
+    .Zip(thrill::api::NoRebalanceTag, new_cluster_ids_times_size, [](const NodeInfo & node_cluster, uint32_t new_cluster_id) { return NodeInfo { node_cluster.id, new_cluster_id }; })
     .Sort(
       [](const NodeInfo & node_cluster1, const NodeInfo & node_cluster2) {
         return node_cluster1.id < node_cluster2.id;
@@ -216,6 +222,8 @@ int main(int, char const *argv[]) {
           uint32_t tail, head;
 
           if (line_stream >> tail >> head) {
+            tail--;
+            head--;
             emit(Edge { tail, head });
             emit(Edge { head, tail });
           } else {
@@ -226,7 +234,7 @@ int main(int, char const *argv[]) {
         return (e1.tail == e2.tail && e1.head < e2.head) || (e1.tail < e2.tail);
       });
 
-    louvain(edges);
+    louvain(edges).Print("Clusters");
   });
 }
 
