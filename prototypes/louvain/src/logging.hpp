ce4cb6e26d27a4d4d6f41f3bc29ac0af5445b862
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-01-10 14:12:14 +0100

    remove unnecessary flush

--- prototypes/louvain/src/logging.hpp
+++ prototypes/louvain/src/logging.hpp
@@ -10,7 +10,6 @@ uint64_t id_counter = 0;
 template<class KeyType, class ValueType>
 void report(const std::string & type, const uint64_t id, const KeyType & key, const ValueType & value) {
   std::cout << type << '/' << id << '/' << key << ": " << value << std::endl;
-  std::cout.flush();
 }
 
 uint64_t getUnusedId() {
