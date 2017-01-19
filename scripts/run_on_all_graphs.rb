67f3904f512e09c67f4a96179203e3687cceea47
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-01-19 12:29:03 +0100

    ruby script robustness

--- scripts/run_on_all_graphs.rb
+++ scripts/run_on_all_graphs.rb
@@ -7,7 +7,10 @@ graphs = Dir.glob(File.join(ARGV[0], '**/*.graph'))
     name = graph.split('/').last.split('.').first
     ground_proof = "#{graph[0..-6]}part"
     puts '#' * 64, name, '#' * 64
-    `./louvain #{graph} #{File.exist?(ground_proof) ? ground_proof : ''} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ../../../scripts/report_to_json.rb`
+    begin
+      `./louvain #{graph} #{File.exist?(ground_proof) ? ground_proof : ''} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ../../../scripts/report_to_json.rb`
+    rescue Exception => _
+    end
     if File.exist?('core')
       `mv core #{"core-#{name}-#{i}"}`
     end
