a544039be741562f348dc61b72c244dc5d993f83
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-02-04 11:44:06 +0100

    update umbrella script

--- scripts/run_on_all_graphs.rb
+++ scripts/run_on_all_graphs.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/env ruby
 
-graphs = Dir.glob(File.join(ARGV[0], '**/*.graph'))
+graphs = Dir.glob(File.join(ARGV[0], '**/*.graph')) + Dir.glob(File.join(ARGV[0], '**/*.txt'))
 
 5.times do |i|
   graphs.each do |graph|
     name = graph.split('/').last.split('.').first
-    ground_proof = "#{graph[0..-6]}part"
+    ground_proof = "#{graph}.cmty"
+
+    args = ''
+    args << ' -g ' << ground_proof if File.exist?(ground_proof)
+    args << ' -f ' if graph.end_with?('.txt')
+
     puts '#' * 64, name, '#' * 64
     begin
-      `./louvain #{graph} #{File.exist?(ground_proof) ? ground_proof : ''} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ../../../scripts/report_to_json.rb`
+      `./louvain #{graph} #{args} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ../../../scripts/report_to_json.rb`
     rescue Exception => _
     end
     if File.exist?('core')
