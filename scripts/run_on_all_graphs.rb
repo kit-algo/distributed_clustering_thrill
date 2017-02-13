2c869e1321247a493e54707d3f58b13eb60d47e1
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-02-13 18:24:41 +0100

    extend wrapper script to include externaly generated partitions

--- scripts/run_on_all_graphs.rb
+++ scripts/run_on_all_graphs.rb
@@ -1,6 +1,21 @@
 #!/usr/bin/env ruby
 
+require 'json'
+
 graphs = Dir.glob(File.join(ARGV[0], '**/*.graph')) + Dir.glob(File.join(ARGV[0], '**/*.txt'))
+
+json_files = Dir.glob(File.join(ARGV[1], '*.json'))
+data = {}
+json_files.each do |file|
+  JSON.parse(File.read(file)).each do |item_key, items|
+    if data[item_key]
+      data[item_key].merge!(items)
+    else
+      data[item_key] = items
+    end
+  end
+end
+
 
 5.times do |i|
   graphs.each do |graph|
@@ -10,6 +25,8 @@ graphs = Dir.glob(File.join(ARGV[0], '**/*.graph')) + Dir.glob(File.join(ARGV[0]
     args = ''
     args << ' -g ' << ground_proof if File.exist?(ground_proof)
     args << ' -f ' if graph.end_with?('.txt')
+
+    args << data['program_run'].select { |key, run| run['graph'] == graph }.map { |key, run| "#{run['output']},#{key}" }.join(" ")
 
     puts '#' * 64, name, '#' * 64
     begin
