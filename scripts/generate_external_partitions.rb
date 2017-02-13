312f99f97b4b908c05161afeca287c024ffe17db
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-02-13 19:03:35 +0100

    more bugfixing

--- scripts/generate_external_partitions.rb
+++ scripts/generate_external_partitions.rb
@@ -8,18 +8,18 @@ graphs.each do |graph|
   [4, 32, 128, 1024].each do |k|
     start = Time.now
     `./kaffpa #{graph} --k=#{k} --preconfiguration=ecosocial --output_filename=#{graph}#{k}.kahip.part`
-    puts "program_run/#{logging_id_counter}/program_run/time: #{Time.now - start}"
-    puts "program_run/#{logging_id_counter}/program_run/binary: kaffpa"
-    puts "program_run/#{logging_id_counter}/program_run/graph: #{graph}"
-    puts "program_run/#{logging_id_counter}/program_run/output: #{graph}#{k}.kahip.part"
+    puts "program_run/#{logging_id_counter}/time: #{Time.now - start}"
+    puts "program_run/#{logging_id_counter}/binary: kaffpa"
+    puts "program_run/#{logging_id_counter}/graph: #{graph}"
+    puts "program_run/#{logging_id_counter}/output: #{graph}#{k}.kahip.part"
     logging_id_counter += 1
 
     start = Time.now
     `./label_prop_partitioning #{graph} -k #{k} -o #{graph}#{k}.lp.part`
-    puts "program_run/#{logging_id_counter}/program_run/time: #{Time.now - start}"
-    puts "program_run/#{logging_id_counter}/program_run/binary: label_prop_partitioning"
-    puts "program_run/#{logging_id_counter}/program_run/graph: #{graph}"
-    puts "program_run/#{logging_id_counter}/program_run/output: #{graph}#{k}.lp.part"
+    puts "program_run/#{logging_id_counter}/time: #{Time.now - start}"
+    puts "program_run/#{logging_id_counter}/binary: label_prop_partitioning"
+    puts "program_run/#{logging_id_counter}/graph: #{graph}"
+    puts "program_run/#{logging_id_counter}/output: #{graph}#{k}.lp.part"
     logging_id_counter += 1
   end
 end
