9d4e1b9e526bf04a854f4745a3b1e111eebee674
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-03-21 15:15:43 +0100

    fix algo name

--- prototypes/thrill_louvain/src/node_based_local_moving.cpp
+++ prototypes/thrill_louvain/src/node_based_local_moving.cpp
@@ -2,7 +2,7 @@
 #include "thrill_louvain.hpp"
 
 int main(int argc, char const *argv[]) {
-  return Louvain::performAndEvaluate(argc, argv, "thrill partitioned louvain", [](const auto& graph) {
+  return Louvain::performAndEvaluate(argc, argv, "thrill node based fully distributed local moving", [](const auto& graph) {
     return Louvain::louvain(graph, [](const auto& graph) {
       return LocalMoving::distributedLocalMoving(graph, 32);
     });
