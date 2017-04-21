899bb7fa16ade6b783faac788c55095b0c8e86d8
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-04-21 21:30:02 +0200

    preprocess the missing graph

--- scripts/prepro_all.rb
+++ scripts/prepro_all.rb
@@ -17,20 +17,20 @@
 # ]
 
 graphs = [
-  ['graphs/hypercubegraph23.graph'],
-  ['graphs/uk-2002.metis.graph'],
+  # ['graphs/hypercubegraph23.graph'],
+  # ['graphs/uk-2002.metis.graph'],
   ['graphs/uk-2007-05.metis.graph'],
-  ['graphs/com-friendster.txt'],
-  ['graphs/com-lj.ungraph.txt'],
-  ['graphs/com-orkut.ungraph.txt']
+  # ['graphs/com-friendster.txt'],
+  # ['graphs/com-lj.ungraph.txt'],
+  # ['graphs/com-orkut.ungraph.txt']
 ]
 
 small_graphs = [
   # ['graphs/graph_50_10000_mu_0.5_100000.bin', 'graphs/part_50_10000_mu_0.5_100000.bin'],
   # ['graphs/graph_50_10000_mu_0.5_1000000.bin', 'graphs/part_50_10000_mu_0.5_1000000.bin'],
-  ['graphs/in-2004.metis.graph'],
+  # ['graphs/in-2004.metis.graph'],
   # ['graphs/com-youtube.ungraph.txt'],
-  ['graphs/com-amazon.ungraph.txt']
+  # ['graphs/com-amazon.ungraph.txt']
 ]
 
 graphs.each do |graph|
