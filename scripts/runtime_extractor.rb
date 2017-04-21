28b6cc6dc4a3acdad721b756f9bb714f75f555a6
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-04-21 11:49:51 +0200

    add script to parse runtime without IO

---
+++ scripts/runtime_extractor.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+
+ARGV.each do |job_output_file|
+  job_id = job_output_file.match(/job_uc1_(\d+)\.out/)[1]
+
+  start = Float::INFINITY
+  final_stage = 0
+  ending = 0
+
+  Dir.glob("*-log-#{job_id}-*.json").each do |json_log|
+    File.open(json_log).each do |line|
+      event = JSON.parse(line)
+      start = event['ts'] if event['id'] == 9 && event['event'] == 'execute-start' && event['ts'] < start
+      if event['id'] && event['id'] >= final_stage && event['label'] == 'ZipWithIndex' && event['event'] == 'pushdata-done'
+        final_stage = event['id']
+        ending = event['ts'] if event['ts'] > ending
+      end
+    end
+  end
+
+  open(job_output_file, 'a') do |f|
+    f.puts "#LOG# algorithm_run/1/runtime: #{(ending - start) / 1000}"
+  end
+end
