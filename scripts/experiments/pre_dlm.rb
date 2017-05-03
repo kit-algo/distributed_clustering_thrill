287df2b7df48cc2233ef67fd7ea3bfa3c7e9acce
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-05-03 12:29:42 +0200

    pre exp scripts

---
+++ scripts/experiments/pre_dlm.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+bins = [
+  'dlm_cluster_1',
+  'dlm_cluster_2',
+  'dlm_cluster_4',
+  'dlm_cluster_8',
+  'dlm_cluster_dyn',
+  'dlm_moved_1',
+  'dlm_moved_2',
+  'dlm_moved_4',
+  'dlm_moved_8',
+  'dlm_moved_dyn'
+]
+
+bins.each do |bin|
+  10.times do
+    `msub -v GRAPH=graphs/mu-04/graph_50_10000_mu_0.4_10000000-preprocessed-*.bin -v CLUSTERING=clusterings/pre/dlm -v EXECUTABLE=/home/kit/iti/kp0036/code/prototypes/thrill_louvain/release/#{bin} ~/code/scripts/moab/pre_exp_dlm.sh`
+  end
+end
---
+++ scripts/moab/pre_exp_dlm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#MSUB -N distributed_local_moving
+#MSUB -l walltime=01:00:00
+#MSUB -l nodes=4:ppn=16
+#MSUB -l mem=256000mb
+#MSUB -q multinode
+#MSUB -v THRILL_WORKERS_PER_HOST=16
+#MSUB -v MPI_MODULE=mpi/openmpi/2.0-gnu-5.2
+
+export THRILL_LOG="dlm-log-${MOAB_JOBID}"
+module load ${MPI_MODULE}
+MPIRUN_OPTIONS="--bind-to core --map-by node:PE=$((MOAB_PROCCOUNT / MOAB_NODECOUNT)) -report-bindings"
+
+echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} MPI-tasks and ${THRILL_WORKERS_PER_HOST} threads"
+
+result_id=$(ruby -e "require 'securerandom'; puts SecureRandom.uuid")
+startexe="mpirun -n ${MOAB_NODECOUNT} ${MPIRUN_OPTIONS} ${EXECUTABLE}"
+echo $startexe "$GRAPH" "$CLUSTERING-$MOAB_JOBID-@@@@-#####.bin,$result_id"
+exec $startexe "$GRAPH" "$CLUSTERING-$MOAB_JOBID-@@@@-#####.bin,$result_id"
