02026de4238c98244da029210aa39ed8381fe33c
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-05-08 16:20:11 +0200

    prepro small ones again

--- scripts/prepro_all.rb
+++ scripts/prepro_all.rb
@@ -32,26 +32,26 @@ small_graphs = [
   # ['graphs/in-2004.metis.graph'],
   # ['graphs/com-youtube.ungraph.txt'],
   # ['graphs/com-amazon.ungraph.txt']
-  # ['graphs/mu-04/graph_50_10000_mu_0.4_100000-sorted*.bin', 'pgraphs/mu-04/art_50_10000_mu_0.4_100000-sorted.bin'],
-  # ['graphs/mu-04/graph_50_10000_mu_0.4_1000000.bin', 'graphs/mu-04/part_50_10000_mu_0.4_2000000.bin']
+  ['graphs/mu-04/graph_50_10000_mu_0.4_100000-sorted*.bin', 'graphs/mu-04/art_50_10000_mu_0.4_100000-sorted.bin'],
+  ['graphs/mu-04/graph_50_10000_mu_0.4_1000000.bin', 'graphs/mu-04/part_50_10000_mu_0.4_1000000.bin']
 ]
 
 large_graphs = [
   # ['graphs/mu-04/graph_50_10000_mu_0.4_100000000.bin', 'graphs/mu-04/part_50_10000_mu_0.4_100000000.bin'],
-  ['graphs/mu-04/graph_50_10000_mu_0.4_200000000-sorted.bin', 'graphs/mu-04/part_50_10000_mu_0.4_200000000-sorted.bin'],
+  # ['graphs/mu-04/graph_50_10000_mu_0.4_200000000-sorted.bin', 'graphs/mu-04/part_50_10000_mu_0.4_200000000-sorted.bin'],
 ]
 
 graphs.each do |graph|
-  groundtruth_option = graph.size == 2 ? "-v GROUNDTRUTH=#{graph[1]}" : ''
-  puts graph[0], `msub -v GRAPH=#{graph[0]} #{groundtruth_option} ~/code/scripts/moab/preprocess.sh`
+  groundtruth_option = graph.size == 2 ? "-v GROUNDTRUTH=#{ENV['HOME']}/#{graph[1]}" : ''
+  puts graph[0], `msub -v GRAPH=#{ENV['HOME']}/#{graph[0]} #{groundtruth_option} ~/code/scripts/moab/preprocess.sh`
 end
 
 small_graphs.each do |graph|
-  groundtruth_option = graph.size == 2 ? "-v GROUNDTRUTH=#{graph[1]}" : ''
-  puts graph[0], `msub -v GRAPH=#{graph[0]} #{groundtruth_option} ~/code/scripts/moab/preprocess_s.sh`
+  groundtruth_option = graph.size == 2 ? "-v GROUNDTRUTH=#{ENV['HOME']}/#{graph[1]}" : ''
+  puts graph[0], `msub -v GRAPH=#{ENV['HOME']}/#{graph[0]} #{groundtruth_option} ~/code/scripts/moab/preprocess_s.sh`
 end
 
 large_graphs.each do |graph|
-  groundtruth_option = graph.size == 2 ? "-v GROUNDTRUTH=#{graph[1]}" : ''
-  puts graph[0], `msub -v GRAPH=#{graph[0]} #{groundtruth_option} ~/code/scripts/moab/preprocess_l.sh`
+  groundtruth_option = graph.size == 2 ? "-v GROUNDTRUTH=#{ENV['HOME']}/#{graph[1]}" : ''
+  puts graph[0], `msub -v GRAPH=#{ENV['HOME']}/#{graph[0]} #{groundtruth_option} ~/code/scripts/moab/preprocess_l.sh`
 end
