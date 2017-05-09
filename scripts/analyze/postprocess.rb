76b16422e154902048d2d8cebde95f3d50722f92
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-05-09 12:16:19 +0200

    add some logging to postprocessing

--- scripts/analyze/postprocess.rb
+++ scripts/analyze/postprocess.rb
@@ -4,11 +4,16 @@ scripts_dir = File.expand_path(File.dirname(__FILE__))
 
 Dir.chdir(ARGV.first) do
   `mkdir reports`
+  puts 'fix json'
   `sed -i 's/:inf/:Infinity/g;s/:nan/:NaN/g;s/:-nan/:-NaN/g' *.json`
+  puts 'extract runtimes'
   `#{scripts_dir}/runtime_extractor.rb #{Dir.glob("job_*.out").join(' ')}`
+  puts 'generate reports'
   Dir.glob("job_*.out").each do |job_file|
     `MA_RESULT_OUTPUT_DIR=#{Dir.join(ARGV.first, 'reports')} #{scripts_dir}/report_to_json.rb #{job_file}`
   end
+  puts 'generate zip'
   `zip reports.zip reports/*`
   `rm -r reports`
+  puts 'done'
 end
