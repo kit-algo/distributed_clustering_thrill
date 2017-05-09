40a87e49c2fb25a28d32ce9c7079fa777b8a9e30
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-05-09 12:04:58 +0200

    glob fix in postprocessing

--- scripts/analyze/postprocess.rb
+++ scripts/analyze/postprocess.rb
@@ -4,7 +4,7 @@ scripts_dir = File.expand_path(File.dirname(__FILE__))
 
 Dir.chdir(ARGV.first) do
   `mkdir reports`
-  `sed -i 's/:inf/:Infinity/g;s/:nan/:NaN/g;s/:-nan/:-NaN/g' log-*.json`
+  `sed -i 's/:inf/:Infinity/g;s/:nan/:NaN/g;s/:-nan/:-NaN/g' *.json`
   `#{scripts_dir}/runtime_extractor.rb #{Dir.glob("job_*.out").join(' ')}`
   Dir.glob("job_*.out").each do |job_file|
     `MA_RESULT_OUTPUT_DIR=#{Dir.join(ARGV.first, 'reports')} #{scripts_dir}/report_to_json.rb #{job_file}`
