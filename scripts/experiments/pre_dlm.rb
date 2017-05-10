73edcc15053458b51f641c15dd1b244b09ed4dd4
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-05-10 10:57:38 +0200

    one more time for the dlm pre

--- scripts/experiments/pre_dlm.rb
+++ scripts/experiments/pre_dlm.rb
@@ -13,8 +13,16 @@ bins = [
   'dlm_moved_dyn'
 ]
 
-bins.each do |bin|
-  4.times do
-    puts `msub -v GRAPH=/home/kit/iti/kp0036/graphs/mu-04/graph_50_10000_mu_0.4_10000000-preprocessed-*.bin -v CLUSTERING=clusterings/dlm -v EXECUTABLE=/home/kit/iti/kp0036/code/prototypes/thrill_louvain/release/#{bin} /home/kit/iti/kp0036/code/scripts/moab/pre_exp_dlm.sh`
+graphs = [
+  # '/home/kit/iti/kp0036/graphs/mu-04/graph_50_10000_mu_0.4_10000000-preprocessed-*.bin',
+  '/home/kit/iti/kp0036/graphs/com-friendster-preprocessed-*.bin',
+  '/home/kit/iti/kp0036/graphs/uk-2002.metis-preprocessed-*.bin'
+]
+
+graphs.each do |graph|
+  bins.each do |bin|
+    1.times do
+      puts `msub -v GRAPH=#{graph} -v CLUSTERING=clusterings/dlm -v EXECUTABLE=/home/kit/iti/kp0036/code/prototypes/thrill_louvain/release/#{bin} /home/kit/iti/kp0036/code/scripts/moab/pre_exp_dlm.sh`
+    end
   end
 end
