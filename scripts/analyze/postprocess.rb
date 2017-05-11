b57e2ccae9bc4db60f25f3ea6660daaf9a591292
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-05-11 14:21:26 +0200

    get output from subscript

--- scripts/analyze/postprocess.rb
+++ scripts/analyze/postprocess.rb
@@ -5,9 +5,9 @@ scripts_dir = File.expand_path(File.dirname(__FILE__))
 Dir.chdir(ARGV.first) do
   `mkdir reports`
   puts 'fix json'
-  `sed -i 's/:inf/:Infinity/g;s/:nan/:NaN/g;s/:-nan/:-NaN/g' *.json`
+  # `sed -i 's/:inf/:Infinity/g;s/:nan/:NaN/g;s/:-nan/:-NaN/g' *.json`
   puts 'extract runtimes'
-  `#{scripts_dir}/runtime_extractor.rb #{Dir.glob("job_*.out").join(' ')}`
+  system("#{scripts_dir}/runtime_extractor.rb #{Dir.glob("job_*.out").join(' ')}")
   puts 'generate reports'
   Dir.glob("job_*.out").each do |job_file|
     `MA_RESULT_OUTPUT_DIR=#{File.join(ARGV.first, 'reports')} #{scripts_dir}/report_to_json.rb #{job_file}`
