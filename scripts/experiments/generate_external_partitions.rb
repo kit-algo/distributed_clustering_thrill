d9e1db4cc6fbd54fd1297f400414045046e41055
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-05-12 14:50:57 +0200

    fix binary name

--- scripts/experiments/generate_external_partitions.rb
+++ scripts/experiments/generate_external_partitions.rb
@@ -7,7 +7,7 @@ logging_id_counter = 0
 graphs.each do |graph|
   [4, 16, 64, 256, 1024].each do |k|
     start = Time.now
-    system "./label_prop_partitioning #{graph} -k #{k} -o #{File.join(ARGV[1], "#{graph}#{k}.lp.part")}"
+    system "./label_prop #{graph} -k #{k} -o #{File.join(ARGV[1], "#{graph}#{k}.lp.part")}"
     puts "#LOG# program_run/#{logging_id_counter}/runtime: #{Time.now - start}"
     puts "#LOG# program_run/#{logging_id_counter}/binary: label_prop_partitioning"
     puts "#LOG# program_run/#{logging_id_counter}/graph: #{graph}"
