3fdc6c51c304f29948bf2b6f9b5a1fd34f7ed6ff
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-05-15 11:43:31 +0200

    better times for lp scaling

--- scripts/experiments/lp_scaling.rb
+++ scripts/experiments/lp_scaling.rb
@@ -3,7 +3,7 @@
 graphs = {
   "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_1000000-preprocessed-*.bin" => [:xs, :s],
   "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_10000000-preprocessed-*.bin" => [:s, :m, :l],
-  "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_100000000-preprocessed-*.bin" => [:l, :xl],
+  "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_100000000-preprocessed-*.bin" => [:ll, :xl],
 }
 
 
@@ -27,13 +27,19 @@ node_configs = {
     '-l nodes=4:ppn=28 -v THRILL_WORKERS_PER_HOST=28 -l walltime=01:00:00',
   ],
   l: [
-    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=2  -l walltime=03:00:00',
-    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=5  -l walltime=03:00:00',
-    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=17 -l walltime=03:00:00',
-    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=28 -l walltime=03:00:00',
+    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=2  -l walltime=01:00:00',
+    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=5  -l walltime=01:00:00',
+    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=17 -l walltime=01:00:00',
+    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=28 -l walltime=01:00:00',
+  ],
+  ll: [
+    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=2  -l walltime=05:00:00',
+    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=5  -l walltime=05:00:00',
+    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=17 -l walltime=05:00:00',
+    '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=28 -l walltime=05:00:00',
   ],
   xl: [
-    '-l nodes=16:ppn=28 -v THRILL_WORKERS_PER_HOST=2  -l walltime=02:00:00',
+    '-l nodes=16:ppn=28 -v THRILL_WORKERS_PER_HOST=2  -l walltime=03:00:00',
     '-l nodes=16:ppn=28 -v THRILL_WORKERS_PER_HOST=5  -l walltime=02:00:00',
     '-l nodes=16:ppn=28 -v THRILL_WORKERS_PER_HOST=17 -l walltime=02:00:00',
     '-l nodes=16:ppn=28 -v THRILL_WORKERS_PER_HOST=28 -l walltime=02:00:00',
