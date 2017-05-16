db498de439eda4c218d9b57c618b52309611420f
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-05-16 12:19:13 +0200

    script to cluster all lfr param variants

---
+++ scripts/experiments/lfr_params.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+graphs = Dir.glob(File.join(ARGV[0], '**/*.graph')) # + Dir.glob(File.join(ARGV[0], '**/*.txt'))
+`mkdir #{ARGV[1]}/seq_louvain`
+`mkdir #{ARGV[1]}/dlm`
+
+graphs.each do |graph|
+  name = graph.split('/').last.split('.').first
+  2.times do |i|
+    cmd = "./seq_louvain #{graph} -o #{ARGV[1]}/seq_louvain/#{name}.run#{i}.part"
+    puts cmd
+    begin
+      system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
+    rescue Exception => _
+    end
+  end
+
+  cmd = "./node_based_local_moving #{graph} #{ARGV[1]}/dlm/#{name}.part"
+  puts cmd
+  begin
+    system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
+  rescue Exception => _
+  end
+end
