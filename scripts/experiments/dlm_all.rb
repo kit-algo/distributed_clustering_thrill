e572144846542b23db56da7f8177d7cfa08cf4f0
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-05-16 14:59:16 +0200

    update dlm_all exp

--- scripts/experiments/dlm_all.rb
+++ scripts/experiments/dlm_all.rb
@@ -1,19 +1,20 @@
 #!/usr/bin/env ruby
 
 graphs = {
-  'graphs/graph_50_10000_mu_0.5_100000-preprocessed-*.bin' => [:fast, [:s, :m, :xl]],
-  'graphs/graph_50_10000_mu_0.5_1000000-preprocessed-*.bin' => [:standard, [:s, :m, :xl]],
-  'graphs/graph_50_10000_mu_0.5_10000000-preprocessed-*.bin' => [:standard, [:s, :m, :xl]],
-  # 'graphs/graph_50_10000_mu_0.5_100000000-preprocessed-*.bin' => [:slow, [:m, :xl]],
-  'graphs/hypercubegraph23-preprocessed-*.bin' => [:slow, [:s, :m, :xl]],
-  'graphs/uk-2002.metis-preprocessed-*.bin' => [:standard, [:s, :m, :xl]],
-  # 'graphs/uk-2007-05.metis-preprocessed-*.bin' => [:slow, [:s, :m, :xl]],
-  'graphs/in-2004.metis-preprocessed-*.bin' => [:standard, [:s, :m, :xl]],
-  'graphs/com-friendster-preprocessed-*.bin' => [:standard, [:s, :m, :xl]],
-  'graphs/com-lj.ungraph-preprocessed-*.bin' => [:standard, [:s, :m, :xl]],
-  'graphs/com-orkut.ungraph-preprocessed-*.bin' => [:standard, [:s, :m, :xl]],
-  'graphs/com-youtube.ungraph-preprocessed-*.bin' => [:fast, [:s, :m, :xl]],
-  'graphs/com-amazon.ungraph-preprocessed-*.bin' => [:fast, [:s, :m, :xl]]
+  "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_100000-sorted-preprocessed-*.bin" => [:fast, [:xs, :s]],
+  "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_1000000-preprocessed-*.bin" => [:fast, [:xs, :s, :m]],
+  "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_10000000-preprocessed-*.bin" => [:standard, [:s, :m, :l]],
+  "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_100000000-preprocessed-*.bin" => [:slow, [:m, :l, :xl]],
+  "#{ENV['HOME']}/graphs/mu-04/graph_50_10000_mu_0.4_200000000-sorted-preprocessed-*.bin" => [:slow, [:l, :xl, :xxl]],
+  "#{ENV['HOME']}/graphs/hypercubegraph23-preprocessed-*.bin" => [:standard, [:s, :m, :l]],
+  "#{ENV['HOME']}/graphs/uk-2002.metis-preprocessed-*.bin" => [:standard, [:s, :m, :l]],
+  "#{ENV['HOME']}/graphs/uk-2007-05.metis-preprocessed-*.bin" => [:slow, [:m, :l, :xl]],
+  "#{ENV['HOME']}/graphs/in-2004.metis-preprocessed-*.bin" => [:standard, [:xs, :s, :m]],
+  "#{ENV['HOME']}/graphs/com-friendster-preprocessed-*.bin" => [:standard, [:m, :l, :xl]],
+  "#{ENV['HOME']}/graphs/com-lj.ungraph-preprocessed-*.bin" => [:standard, [:s, :m, :l]],
+  "#{ENV['HOME']}/graphs/com-orkut.ungraph-preprocessed-*.bin" => [:standard, [:s, :m, :l]],
+  "#{ENV['HOME']}/graphs/com-youtube.ungraph-preprocessed-*.bin" => [:fast, [:xs, :s]],
+  "#{ENV['HOME']}/graphs/com-amazon.ungraph-preprocessed-*.bin" => [:fast, [:xs, :s]]
 }
 
 time_configs = {
@@ -23,18 +24,22 @@ time_configs = {
 }
 
 node_configs = {
-  xs: '-l nodes=1:ppn=2 -v THRILL_WORKERS_PER_HOST=2 -l mem=64gb -q singlenode',
-  s: '-l nodes=1:ppn=5 -v THRILL_WORKERS_PER_HOST=5 -l mem=64gb -q singlenode',
-  ms: '-l nodes=2:ppn=5 -v THRILL_WORKERS_PER_HOST=5 -l mem=128gb -q multinode',
-  m: '-l nodes=4:ppn=5 -v THRILL_WORKERS_PER_HOST=5 -l mem=256gb -q multinode',
-  l: '-l nodes=8:ppn=5 -v THRILL_WORKERS_PER_HOST=5 -l mem=512gb -q multinode',
-  xl: '-l nodes=16:ppn=5 -v THRILL_WORKERS_PER_HOST=5 -l mem=1024gb -q multinode',
+  xs: '-l nodes=2:ppn=28 -v THRILL_WORKERS_PER_HOST=17',
+  s:  '-l nodes=2:ppn=28 -v THRILL_WORKERS_PER_HOST=17',
+  m:  '-l nodes=4:ppn=28 -v THRILL_WORKERS_PER_HOST=17',
+  l:  '-l nodes=8:ppn=28 -v THRILL_WORKERS_PER_HOST=17',
+  xl: '-l nodes=16:ppn=28 -v THRILL_WORKERS_PER_HOST=17',
+  xxl: '-l nodes=32:ppn=28 -v THRILL_WORKERS_PER_HOST=17',
 }
 
 
 graphs.each do |graph, configs|
-  configs[1].each do |node_config|
-    command_result = `msub -v GRAPH=#{graph} -v CLUSTERING=clusterings/dlm #{time_configs[configs[0]]} #{node_configs[node_config]} ~/code/scripts/moab/no_default_dlm.sh`
-    puts "#{graph} #{configs[0]} #{node_config} #{command_result.strip}"
+  ARGV.each do |prio|
+    node_config = configs[1][-prio.to_i]
+    if node_config
+      moab = (size == :xs ? 'fake_single_dlm.sh' : 'no_default_dlm.sh')
+      command_result = `msub -v GRAPH=#{graph} -v CLUSTERING=clusterings/dlm #{time_configs[configs[0]]} #{node_configs[node_config]} ~/code/scripts/moab/#{moab}`
+      puts "#{graph} #{configs[0]} #{node_config} #{command_result.strip}"
+    end
   end
 end
