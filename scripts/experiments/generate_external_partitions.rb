87a7ade42b9d43e26f84305b992ed662a1d933a8
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2017-05-17 09:20:42 +0200

    more script updates

--- scripts/experiments/generate_external_partitions.rb
+++ scripts/experiments/generate_external_partitions.rb
@@ -5,11 +5,12 @@ logging_id_counter = 0
 ARGV.each do |graph|
   [4, 16, 64, 256, 1024].each do |k|
     start = Time.now
-    system "./label_prop #{graph} -k #{k} -o #{File.join(File.dirname(graph), 'partitionings', "#{graph}#{k}.lp.part")}"
+    output = File.join(File.dirname(Dir.glob(graph).first), 'partitionings', "#{graph.gsub('*', '')}#{k}.lp.part")
+    system "./label_prop #{graph} -k #{k} -o #{output}"
     puts "#LOG# program_run/#{logging_id_counter}/runtime: #{Time.now - start}"
     puts "#LOG# program_run/#{logging_id_counter}/binary: label_prop_partitioning"
     puts "#LOG# program_run/#{logging_id_counter}/graph: #{graph}"
-    puts "#LOG# program_run/#{logging_id_counter}/output: #{File.join(ARGV[1], "#{graph}#{k}.lp.part")}"
+    puts "#LOG# program_run/#{logging_id_counter}/output: #{output}"
     logging_id_counter += 1
   end
 end
