38db08cfcea1d12dbaf3aeeaa8609155a85fcfb2
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-05-23 09:33:05 +0200

    no join modularity calculation

--- prototypes/thrill_louvain/src/distributed_clustering_analyser.cpp
+++ prototypes/thrill_louvain/src/distributed_clustering_analyser.cpp
@@ -8,12 +8,15 @@
 #include "seq_louvain/cluster_store.hpp"
 #include "logging.hpp"
 
+using int128_t = __int128_t;
+
 int main(int, char const *argv[]) {
   return thrill::Run([&](thrill::Context& context) {
     context.enable_consume();
 
     std::pair<std::string, std::string> algo_clustering_input = Logging::parse_input_with_logging_id(argv[2]);
-    auto node_clusters = Input::readClustering(algo_clustering_input.first, context);
+    auto node_clusters = Input::readClustering(algo_clustering_input.first, context)
+      .Sort([](const NodeCluster& nc1, const NodeCluster& nc2) { return nc1.first < nc2.first; });
 
     auto graph = Input::readToNodeGraph(argv[1], context);
 
@@ -32,30 +35,52 @@ int main(int, char const *argv[]) {
       }
     }
 
-    Logging::Id algorithm_run_id = 0;
-    if (context.my_rank() == 0) {
-      algorithm_run_id = Logging::getUnusedId();
-    }
-    auto result = Louvain::louvain(graph, algorithm_run_id, [node_count = graph.node_count, &node_clusters](const auto& inner_graph, Logging::Id) {
-      using NodeType = typename decltype(inner_graph.nodes)::ValueType;
+    size_t cluster_count = node_clusters.Keep().Map([](const NodeCluster& node_cluster) { return node_cluster.second; }).Uniq().Size();
 
-      if (std::is_same<NodeType, NodeWithLinks>::value) {
-        return node_clusters
-          .Sort([](const NodeCluster& nc1, const NodeCluster& nc2) { return nc1.first < nc2.first; })
-          .Zip(inner_graph.nodes,
-            [](const NodeCluster& node_cluster, const NodeType& node) {
-              assert(node.id == node_cluster.first);
-              return std::make_pair(node, node_cluster.second);
-            });
-      } else {
-        return inner_graph.nodes
-          .Map([](const NodeType& node) { return std::make_pair(node, node.id); })
-          .Collapse();
-      }
-    });
+    auto edges = graph.nodes
+      .Zip(node_clusters,
+        [](const NodeWithLinks& node, const NodeCluster& node_cluster) {
+          assert(node.id == node_cluster.first);
+          return std::make_pair(node, node_cluster.second);
+        })
+      .template FlatMap<Edge>(
+        [](const std::pair<NodeWithLinks, ClusterId>& node_cluster, auto emit) {
+          for (const auto& link : node_cluster.first.links) {
+            emit(Edge { link.target, node_cluster.second });
+          }
+        });
 
-    size_t cluster_count = result.second.node_count;
-    double modularity = Modularity::modularity(result.second);
+    auto cluster_degrees_and_inside_weights = edgesToNodes(edges, graph.node_count)
+      .Zip(node_clusters,
+        [](const NodeWithLinks& node, const NodeCluster& node_cluster) {
+          assert(node.id == node_cluster.first);
+          return std::make_pair(node, node_cluster.second);
+        })
+      .template FlatMap<Edge>(
+        [](const std::pair<NodeWithLinks, ClusterId>& node_cluster, auto emit) {
+          for (const auto& link : node_cluster.first.links) {
+            emit(Edge { node_cluster.second, link.target });
+          }
+        })
+      .Map([](const Edge& edge) { return WeightedEdge { edge.tail, edge.head, edge.getWeight() }; })
+      .ReduceByKey(
+        [](const WeightedEdge& edge) { return Util::combine_u32ints(edge.tail, edge.head); },
+        [](const WeightedEdge& edge1, const WeightedEdge& edge2) { return WeightedEdge { edge1.tail, edge1.head, edge1.weight + edge2.weight }; })
+      .Map([](const WeightedEdge& edge) { return std::make_pair(edge.tail, std::pair<uint64_t, uint64_t>(edge.getWeight(), edge.tail == edge.head ? edge.getWeight() : 0)); })
+      .ReducePair(
+        [](const std::pair<uint64_t, uint64_t>& weights1, const std::pair<uint64_t, uint64_t>& weights2) {
+          return std::make_pair(weights1.first + weights2.first, weights1.second + weights2.second);
+        })
+      .Map(
+        [](const std::pair<ClusterId, std::pair<uint64_t, uint64_t>>& weights) {
+          return std::make_pair(int128_t(weights.second.first) * int128_t(weights.second.first), weights.second.second);
+        })
+      .AllReduce(
+        [](const std::pair<int128_t, uint64_t>& weights1, const std::pair<int128_t, uint64_t>& weights2) {
+          return std::make_pair(weights1.first + weights2.first, weights1.second + weights2.second);
+        });
+
+    double modularity = (cluster_degrees_and_inside_weights.second / (2.* graph.total_weight)) - (cluster_degrees_and_inside_weights.first / (4.*graph.total_weight*graph.total_weight));
 
     if (context.my_rank() == 0) {
       Logging::report("clustering", algo_clustering_input.second, "path", algo_clustering_input.first);
