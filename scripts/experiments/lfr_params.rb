c361bec101bfa2509a79148699a482fdd4187dc5
Tim Zeitz <dev.tim.zeitz@gmail.com>
2017-07-20 11:48:21 +0200

    map eq lfr params exp

--- scripts/experiments/lfr_params.rb
+++ scripts/experiments/lfr_params.rb
@@ -6,6 +6,7 @@ graphs = Dir.glob(File.join(ARGV[0], '**/*.graph')) # + Dir.glob(File.join(ARGV[
 `mkdir #{ARGV[1]}/seq_louvain`
 `mkdir #{ARGV[1]}/dlm`
 `mkdir #{ARGV[1]}/plm`
+`mkdir #{ARGV[1]}/dlslm_map_eq`
 
 graphs.each do |graph|
   name = graph.split('/').last.split('.').first
@@ -18,17 +19,26 @@ graphs.each do |graph|
   #   end
   # end
 
-  2.times do |i|
-    cmd = "./node_based_local_moving #{graph} #{ARGV[1]}/dlm/#{name}.run#{i}.part,#{SecureRandom.uuid}"
-    puts cmd
-    begin
-      system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
-    rescue Exception => _
-    end
-  end
+  # 2.times do |i|
+  #   cmd = "./node_based_local_moving #{graph} #{ARGV[1]}/dlm/#{name}.run#{i}.part,#{SecureRandom.uuid}"
+  #   puts cmd
+  #   begin
+  #     system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
+  #   rescue Exception => _
+  #   end
+  # end
+
+  # 3.times do |i|
+  #   cmd = "./partitioned_louvain #{graph} #{ARGV[1]}/plm/#{name}.run#{i}.part,#{SecureRandom.uuid}"
+  #   puts cmd
+  #   begin
+  #     system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
+  #   rescue Exception => _
+  #   end
+  # end
 
   3.times do |i|
-    cmd = "./partitioned_louvain #{graph} #{ARGV[1]}/plm/#{name}.run#{i}.part,#{SecureRandom.uuid}"
+    cmd = "./dlslm_map_eq #{graph} #{ARGV[1]}/dlslm_map_eq/#{name}.run#{i}.part,#{SecureRandom.uuid}"
     puts cmd
     begin
       system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
