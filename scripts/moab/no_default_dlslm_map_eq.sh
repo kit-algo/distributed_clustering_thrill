a3788cdec1fe7a21a144cdaa92187d1731677eb3
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-08-31 16:39:08 +0200

    get rid of mpi - at least for map eq, rest is todo

--- scripts/moab/no_default_dlslm_map_eq.sh
+++ scripts/moab/no_default_dlslm_map_eq.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #MSUB -N synchronous map equation
-#MSUB -l pmem=4500mb
+#MSUB -l pmem=4570mb
 #MSUB -q multinode
 #MSUB -v THRILL_RAM=120GiB
-#MSUB -v MPI_MODULE=mpi/openmpi/2.1-gnu-7.1
 
 export THRILL_LOG="dlslm_map_eq-log-${MOAB_JOBID}"
-module load ${MPI_MODULE}
-MPIRUN_OPTIONS="--bind-to core --map-by node:PE=$((MOAB_PROCCOUNT / MOAB_NODECOUNT)) -report-bindings"
+module load compiler/gnu/7.1
 
 echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} MPI-tasks and ${THRILL_WORKERS_PER_HOST} threads"
 
 result_id=$(ruby -e "require 'securerandom'; puts SecureRandom.uuid")
 executable="$HOME/code/release/dlslm_map_eq"
-startexe="mpirun -n ${MOAB_NODECOUNT} ${MPIRUN_OPTIONS} ${executable}"
+startexe="$HOME/code/lib/thrill/run/slurm/invoke.sh ${executable}"
 echo $startexe "$GRAPH" "$CLUSTERING-$MOAB_JOBID-@@@@-#####.bin,$result_id"
 exec $startexe "$GRAPH" "$CLUSTERING-$MOAB_JOBID-@@@@-#####.bin,$result_id"
