319a34eb8b03cb6998481f2b2d3f8accf0f8d891
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-09-05 16:08:13 +0200

    implement a much faster way to calculate final modularity

--- src/algo/thrill/modularity.hpp
+++ src/algo/thrill/modularity.hpp
@@ -14,6 +14,48 @@ namespace Modularity {
 using NodeCluster = std::pair<NodeId, ClusterId>;
 using int128_t = __int128_t;
 
+template<typename NodeType, typename ClusterDIA>
+double betterModularity(const DiaNodeGraph<NodeType>& graph, const ClusterDIA& clusters) {
+  auto inside_and_total_weights = graph.nodes
+    .Zip(clusters,
+      [](const NodeType& node, const NodeCluster& node_cluster) {
+        assert(node_cluster.first == node.id);
+        return std::make_pair(node, node_cluster.second);
+      })
+    .template FoldByKey<std::vector<NodeType>>(thrill::NoDuplicateDetectionTag,
+      [](const std::pair<NodeType, ClusterId>& node_cluster) { return node_cluster.second; },
+      [](std::vector<NodeType>&& acc, const std::pair<NodeType, ClusterId>& node_cluster) {
+        acc.push_back(node_cluster.first);
+        return std::move(acc);
+      })
+    .Map(
+      [](const std::pair<ClusterId, std::vector<NodeType>>& cluster_nodes) {
+        spp::sparse_hash_set<NodeId> cluster_node_ids;
+        cluster_node_ids.reserve(cluster_nodes.second.size());
+        Weight total_weight = 0;
+        Weight inner_weight = 0;
+        for (const NodeType& node : cluster_nodes.second) {
+          total_weight += node.weightedDegree();
+          cluster_node_ids.insert(node.id);
+        }
+        for (const NodeType& node : cluster_nodes.second) {
+          for (const typename NodeType::LinkType& link : node.links) {
+            if (cluster_node_ids.find(link.target) != cluster_node_ids.end()) {
+              inner_weight += link.getWeight();
+            }
+          }
+        }
+
+        return std::make_pair(inner_weight, int128_t(total_weight) * int128_t(total_weight));
+      })
+    .AllReduce(
+      [](const std::pair<uint64_t, int128_t>& weights1, const std::pair<uint64_t, int128_t>& weights2) {
+        return std::make_pair(weights1.first + weights2.first, weights1.second + weights2.second);
+      });
+
+  return (inside_and_total_weights.first / (2.* graph.total_weight)) - (inside_and_total_weights.second / (4.*graph.total_weight*graph.total_weight));
+}
+
 template<typename Graph>
 double modularity(const Graph& graph, const thrill::DIA<NodeCluster>& clusters) {
   auto cluster_degrees_and_inside_weights = graph.edges
