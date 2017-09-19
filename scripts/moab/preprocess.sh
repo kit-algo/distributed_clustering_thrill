e7d4a884881f86071d463482d4e32307c16421c4
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-09-19 17:44:55 +0200

    update prepro scripts

--- scripts/moab/preprocess.sh
+++ scripts/moab/preprocess.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 #MSUB -N preprocess
-#MSUB -l nodes=4:ppn=17
-#MSUB -l walltime=3:00:00
-#MSUB -l mem=512000mb
+#MSUB -l pmem=4570mb
 #MSUB -q multinode
-#MSUB -v MPI_MODULE=mpi/openmpi/2.0-gnu-5.2
 #MSUB -v THRILL_WORKERS_PER_HOST=17
-#MSUB -v MPIRUN_OPTIONS="--bind-to core --map-by node:PE=17 -report-bindings"
+#MSUB -v THRILL_RAM=120GiB
 
-cat /proc/cpuinfo > cpu-$MOAB_JOBID
-
-module load ${MPI_MODULE}
+module load compiler/gnu/7.1
 
 echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} MPI-tasks and ${THRILL_WORKERS_PER_HOST} threads"
 
-executable="$HOME/code/prototypes/thrill_louvain/release/preprocess"
-startexe="mpirun -n ${MOAB_NODECOUNT} ${MPIRUN_OPTIONS} ${executable}"
+executable="$HOME/code/release/preprocess"
+startexe="$HOME/code/lib/thrill/run/slurm/invoke.sh ${executable}"
 echo $startexe "$GRAPH" "$GROUNDTRUTH"
 exec $startexe "$GRAPH" "$GROUNDTRUTH"
 rm "$GRAPH" "$GROUNDTRUTH"
