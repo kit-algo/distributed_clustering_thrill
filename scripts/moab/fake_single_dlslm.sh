11f7530b4d44e9f77d8087d359d1a140ee2f4ea8
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-09-21 16:36:39 +0200

    cleanup singlenode moab scripts

---
+++ scripts/moab/fake_single_dlslm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#MSUB -N synchronous modularity
+#MSUB -l pmem=4570mb
+#MSUB -l nodes=2:ppn=28
+#MSUB -q multinode
+#MSUB -v THRILL_WORKERS_PER_HOST=16
+#MSUB -v THRILL_RAM=120GiB
+
+module load compiler/gnu/7.1
+
+echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} tasks and ${THRILL_WORKERS_PER_HOST} threads"
+
+result_id=$(ruby -e "require 'securerandom'; puts SecureRandom.uuid")
+executable="$HOME/code/release/dlslm"
+startexe="$HOME/code/lib/thrill/run/slurm/invoke.sh ${executable}"
+echo $startexe "$GRAPH" "$CLUSTERING-$MOAB_JOBID-@@@@-#####.bin,$result_id"
+exec srun -v \
+     --exclusive \
+     --ntasks="1" \
+     --ntasks-per-node="1" \
+     --kill-on-bad-exit \
+     --mem=0 \
+     $startexe "$GRAPH" "$CLUSTERING-$MOAB_JOBID-@@@@-#####.bin,$result_id"
