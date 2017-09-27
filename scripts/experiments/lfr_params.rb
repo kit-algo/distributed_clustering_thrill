c08695ca6fce91783a7abe8ce5fa6a1870fd2f68
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-09-27 17:01:36 +0200

    update for new lfr params exp

--- scripts/experiments/lfr_params.rb
+++ scripts/experiments/lfr_params.rb
@@ -2,47 +2,28 @@
 
 require 'securerandom'
 
-graphs = Dir.glob(File.join(ARGV[0], '**/*.graph')) # + Dir.glob(File.join(ARGV[0], '**/*.txt'))
-`mkdir #{ARGV[1]}/seq_louvain`
-`mkdir #{ARGV[1]}/dlm`
-`mkdir #{ARGV[1]}/plm`
-`mkdir #{ARGV[1]}/dlslm_map_eq`
+algos = ['seq_louvain', 'dlslm_map_eq', 'dlslm_with_seq', 'dlslm']
+
+graphs = Dir.glob(File.join(ARGV[0], '**/*.graph'))
+algos.each do |algo|
+  `mkdir #{ARGV[1]}/#{algo}`
+end
 
 graphs.each do |graph|
   name = graph.split('/').last.split('.').first
-  # 2.times do |i|
-  #   cmd = "./seq_louvain #{graph} -o #{ARGV[1]}/seq_louvain/#{name}.run#{i}.part"
-  #   puts cmd
-  #   begin
-  #     system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
-  #   rescue Exception => _
-  #   end
-  # end
-
-  # 2.times do |i|
-  #   cmd = "./node_based_local_moving #{graph} #{ARGV[1]}/dlm/#{name}.run#{i}.part,#{SecureRandom.uuid}"
-  #   puts cmd
-  #   begin
-  #     system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
-  #   rescue Exception => _
-  #   end
-  # end
-
-  # 3.times do |i|
-  #   cmd = "./partitioned_louvain #{graph} #{ARGV[1]}/plm/#{name}.run#{i}.part,#{SecureRandom.uuid}"
-  #   puts cmd
-  #   begin
-  #     system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
-  #   rescue Exception => _
-  #   end
-  # end
 
   3.times do |i|
-    cmd = "./dlslm_map_eq #{graph} #{ARGV[1]}/dlslm_map_eq/#{name}.run#{i}.part,#{SecureRandom.uuid}"
-    puts cmd
-    begin
-      system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
-    rescue Exception => _
+    algos.each do |algo|
+      if algo == 'seq_louvain'
+        cmd = "./#{algo} #{graph} -o #{ARGV[1]}/#{algo}/#{name}.run#{i}.part"
+      else
+        cmd = "./#{algo} #{graph} #{ARGV[1]}/#{algo}/#{name}.run#{i}.part,#{SecureRandom.uuid}"
+      end
+      puts cmd
+      begin
+        system "#{cmd} | tee /dev/tty | MA_RESULT_OUTPUT_DIR=#{ARGV[1]} ./report_to_json.rb"
+      rescue Exception => _
+      end
     end
   end
 end
