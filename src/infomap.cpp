7946bd437144efa8641ac3d36865c7f8e1ca42f8
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-10-02 14:56:44 +0200

    fix namespacing flag, implement data structure translation

--- src/infomap.cpp
+++ src/infomap.cpp
@@ -20,19 +20,26 @@ int main(int argc, char const *argv[]) {
 
   const Graph& graph = input.getGraph();
 
-  // TODO build graph
+  infomap::Infomap infomap_wrapper("--clu -2");
 
+  for (NodeId node = 0; node < graph.getNodeCount(); node++) {
+    graph.forEachAdjacentNode(node, [&](NodeId neighbor, auto) {
+      infomap_wrapper.addLink(node, neighbor);
+    });
+  }
 
   Logging::Id algo_run_logging_id = Logging::getUnusedId();
   Logging::report("algorithm_run", algo_run_logging_id, "program_run_id", run_id);
   Logging::report("algorithm_run", algo_run_logging_id, "algorithm", "sequential infomap");
 
   thrill::common::StatsTimerBase<true> timer(/* autostart */ true);
-  // TODO call infomap
+  infomap_wrapper.run();
   Logging::report("algorithm_run", algo_run_logging_id, "runtime", timer.Microseconds() / 1000000.0);
 
   ClusterStore clusters(graph.getNodeCount());
-  // TODO translate clusters
+  for (auto leafIt = infomap_wrapper.tree.leafIter(); !leafIt.isEnd(); ++leafIt) {
+    clusters.set(leafIt->originalLeafIndex, leafIt.moduleIndex());
+  }
 
   Logging::Id cluster_logging_id = Louvain::log_clustering(graph, clusters);
   Logging::report("clustering", cluster_logging_id, "source", "computation");
