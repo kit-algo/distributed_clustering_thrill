988ea1c08870a151d3fd3081489e2a300e791d5d
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-10-14 00:18:20 +0200

    new weakscaling with adjusted block size

--- scripts/experiments/weak_scaling.rb
+++ scripts/experiments/weak_scaling.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env ruby
 
 jobs = [
-  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_4000000-sorted-preprocessed-*.bin",   00, 10, 1],
-  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_8000000-sorted-preprocessed-*.bin",   00, 10, 2],
-  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_16000000-sorted-preprocessed-*.bin",  00, 20, 4],
-  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_32000000-sorted-preprocessed-*.bin",  00, 30, 8],
-  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_64000000-sorted-preprocessed-*.bin",  01, 00, 16],
-  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_128000000-sorted-preprocessed-*.bin", 01, 00, 32],
-  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_256000000-sorted-preprocessed-*.bin", 02, 00, 64],
-  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_512000000-sorted-preprocessed-*.bin", 05, 00, 128],
+  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_16000000-sorted-preprocessed-*.bin",  00, 20, 1],
+  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_32000000-sorted-preprocessed-*.bin",  00, 29, 2],
+  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_64000000-sorted-preprocessed-*.bin",  01, 00, 4],
+  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_128000000-sorted-preprocessed-*.bin", 01, 00, 8],
+  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_256000000-sorted-preprocessed-*.bin", 01, 29, 16],
+  ["#{ENV['WORK']}/graphs/graph_50_10000_mu_0.4_512000000-sorted-preprocessed-*.bin", 02, 00, 32],
 ]
 
 `mkdir clusterings`
 
+BLOCK_SIZE = 1024 * 128
+
 jobs.each do |job|
   graph, hours, minutes, nodes = *job
-  puts `msub -v GRAPH=#{graph} -v CLUSTERING=clusterings/mod -l walltime=#{"%02d" % hours}:#{"%02d" % minutes}:00 -l nodes=#{[nodes, 2].max}:ppn=28 #{ENV['HOME']}/code/scripts/moab/#{nodes == 1 ? 'fake_single_' : ''}dlslm.sh`
-  puts `msub -v GRAPH=#{graph} -v CLUSTERING=clusterings/mods -l walltime=#{"%02d" % hours}:#{"%02d" % minutes}:00 -l nodes=#{[nodes, 2].max}:ppn=28 #{ENV['HOME']}/code/scripts/moab/#{nodes == 1 ? 'fake_single_' : ''}dlslm_with_seq.sh`
-  puts `msub -v GRAPH=#{graph} -v CLUSTERING=clusterings/me -l walltime=#{"%02d" % (hours * 2)}:#{"%02d" % (minutes * 2)}:00 -l nodes=#{[nodes, 2].max}:ppn=28 #{ENV['HOME']}/code/scripts/moab/#{nodes == 1 ? 'fake_single_' : ''}dlslm_map_eq.sh`
+  puts `msub -v THRILL_BLOCK_SIZE=#{BLOCK_SIZE} -v GRAPH=#{graph} -v CLUSTERING=clusterings/mod -l walltime=#{"%02d" % hours}:#{"%02d" % minutes}:00 -l nodes=#{[nodes, 2].max}:ppn=28 #{ENV['HOME']}/code/scripts/moab/#{nodes == 1 ? 'fake_single_' : ''}dlslm.sh`
+  puts `msub -v THRILL_BLOCK_SIZE=#{BLOCK_SIZE} -v GRAPH=#{graph} -v CLUSTERING=clusterings/mods -l walltime=#{"%02d" % hours}:#{"%02d" % minutes}:00 -l nodes=#{[nodes, 2].max}:ppn=28 #{ENV['HOME']}/code/scripts/moab/#{nodes == 1 ? 'fake_single_' : ''}dlslm_with_seq.sh`
+  puts `msub -v THRILL_BLOCK_SIZE=#{BLOCK_SIZE} -v GRAPH=#{graph} -v CLUSTERING=clusterings/me -l walltime=#{"%02d" % (hours * 2)}:#{"%02d" % (minutes * 2)}:00 -l nodes=#{[nodes, 2].max}:ppn=28 #{ENV['HOME']}/code/scripts/moab/#{nodes == 1 ? 'fake_single_' : ''}dlslm_map_eq.sh`
 end
