741ae0a485a2cdd65f76c357f458f23b196682ae
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-10-16 15:18:28 +0200

    add scripts to analyse the runs where the analysis failed

---
+++ scripts/moab/distributed_clustering_analyser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#MSUB -N distributed clustering analyser
+#MSUB -l pmem=4570mb
+#MSUB -q multinode
+#MSUB -v THRILL_WORKERS_PER_HOST=8
+#MSUB -v THRILL_RAM=120GiB
+#MSUB -v THRILL_BLOCK_SIZE=131072
+#MSUB -v GLIBCPP_FORCE_NEW=1
+
+module load compiler/gnu/7.1
+
+echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} tasks and ${THRILL_WORKERS_PER_HOST} threads"
+
+executable="$HOME/code/release/distributed_clustering_analyser"
+startexe="$HOME/code/lib/thrill/run/slurm/invoke.sh ${executable}"
+echo $startexe "$GRAPH" "$CLUSTERING"
+exec $startexe "$GRAPH" "$CLUSTERING"
