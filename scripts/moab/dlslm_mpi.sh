e835470e1972a9d0a9ac3ce2e14882e034634b62
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-12-13 15:26:07 +0100

    mess with new mpi

---
+++ scripts/moab/dlslm_mpi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#MSUB -N synchronous modularity mpi
+#MSUB -l pmem=4570mb
+#MSUB -q multinode
+#MSUB -v THRILL_WORKERS_PER_HOST=17
+#MSUB -v THRILL_RAM=120GiB
+#MSUB -v GLIBCPP_FORCE_NEW=1
+
+module load mpi/openmpi/3.0-gnu-7.1
+
+echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} tasks and ${THRILL_WORKERS_PER_HOST} threads"
+
+result_id=$(ruby -e "require 'securerandom'; puts SecureRandom.uuid")
+executable="$HOME/code/release/dlslm"
+mpi_options="--bind-to core --map-by node:PE=17 -report-bindings"
+startexe="mpirun -n ${MOAB_NODECOUNT} ${mpi_options} ${executable}"
+echo $startexe "$GRAPH" "$CLUSTERING-$MOAB_JOBID-@@@@-#####.bin,$result_id"
+exec $startexe "$GRAPH" "$CLUSTERING-$MOAB_JOBID-@@@@-#####.bin,$result_id"
