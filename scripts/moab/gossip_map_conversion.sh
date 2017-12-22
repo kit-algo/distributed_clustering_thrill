0c1cedd04a896472805c4601a73c6e7ac058eaa8
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-12-22 15:44:48 +0100

    gossip map pipeline

---
+++ scripts/moab/gossip_map_conversion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#MSUB -N gossip map graph conversion
+#MSUB -l pmem=4000mb
+#MSUB -q singlenode
+
+module load compiler/gnu/7.1
+
+echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} tasks"
+
+executable="$HOME/code/release/convert_graph_to_gossipmap_binary_edgelist"
+echo $executable ${GRAPH} ${OUT}
+exec $executable ${GRAPH} ${OUT}
