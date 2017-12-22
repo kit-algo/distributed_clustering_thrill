16317dee33635e6bdbde656d70bfb86269c01834
ji4215@kit.edu <ji4215@uc1n996.localdomain>
2017-12-22 15:53:16 +0100

    make script executable

--- scripts/experiments/all_real_gossip_map.rb
+++ scripts/experiments/all_real_gossip_map.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 jobs = [
-  ["#{ENV['HOME']}/graphs/uk-2002.metis-preprocessed-*.bin", 1, 0, 8],
-  ["#{ENV['HOME']}/graphs/uk-2007-05.metis-preprocessed-*.bin", 3, 0, 16],
-  ["#{ENV['HOME']}/graphs/in-2004.metis-preprocessed-*.bin", 1, 0, 4],
-  ["#{ENV['HOME']}/graphs/com-friendster-preprocessed-*.bin", 1, 0, 16],
-  ["#{ENV['HOME']}/graphs/com-lj.ungraph-preprocessed-*.bin", 1, 0, 8],
-  ["#{ENV['HOME']}/graphs/com-orkut.ungraph-preprocessed-*.bin", 1, 0, 8],
+#  ["#{ENV['HOME']}/graphs/uk-2002.metis-preprocessed-*.bin", 1, 0, 8],
+#  ["#{ENV['HOME']}/graphs/uk-2007-05.metis-preprocessed-*.bin", 3, 0, 16],
+#  ["#{ENV['HOME']}/graphs/in-2004.metis-preprocessed-*.bin", 1, 0, 4],
+#  ["#{ENV['HOME']}/graphs/com-friendster-preprocessed-*.bin", 1, 0, 16],
+#  ["#{ENV['HOME']}/graphs/com-lj.ungraph-preprocessed-*.bin", 1, 0, 8],
+#  ["#{ENV['HOME']}/graphs/com-orkut.ungraph-preprocessed-*.bin", 1, 0, 8],
   ["#{ENV['HOME']}/graphs/com-youtube.ungraph-preprocessed-*.bin", 0, 10, 2],
-  ["#{ENV['HOME']}/graphs/com-amazon.ungraph-preprocessed-*.bin", 0, 10, 2],
-  ["#{ENV['HOME']}/graphs/europe.osm-preprocessed-*.bin", 1, 0, 8],
+#  ["#{ENV['HOME']}/graphs/com-amazon.ungraph-preprocessed-*.bin", 0, 10, 2],
+#  ["#{ENV['HOME']}/graphs/europe.osm-preprocessed-*.bin", 1, 0, 8],
 ]
 
 `mkdir clusterings`
