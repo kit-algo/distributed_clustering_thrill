20064dd08bfbcb927be8a3936d7cd185f0bccbb4
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-12-22 15:52:31 +0100

    fix glob expansion

--- scripts/moab/gossip_map_conversion.sh
+++ scripts/moab/gossip_map_conversion.sh
@@ -8,5 +8,5 @@ module load compiler/gnu/7.1
 echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} tasks"
 
 executable="$HOME/code/release/convert_graph_to_gossipmap_binary_edgelist"
-echo $executable ${GRAPH} ${OUT}
-exec $executable ${GRAPH} ${OUT}
+echo $executable "${GRAPH}" ${OUT}
+exec $executable "${GRAPH}" ${OUT}
