7f8bfa08326d83bb2f6c79548ac0b5d852cfd75e
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-12-23 07:35:05 +0100

    add moab job id to gossipmap output

--- scripts/moab/gossip_map.sh
+++ scripts/moab/gossip_map.sh
@@ -12,5 +12,5 @@ echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} 
 mpi_options="--bind-to core --map-by node:PE=16 -report-bindings"
 executable="$HOME/gossip_map/release/apps/GossipMap/GossipMap"
 startexe="mpirun -n ${MOAB_NODECOUNT} ${mpi_options} ${executable}"
-echo $startexe --format bintsv4 --graph ${GRAPH} --prefix ${OUT_PREFIX} --ncpus 16
-exec $startexe --format bintsv4 --graph ${GRAPH} --prefix ${OUT_PREFIX} --ncpus 16
+echo $startexe --format bintsv4 --graph ${GRAPH} --prefix "${OUT_PREFIX}-${MOAB_JOBID}" --ncpus 16
+exec $startexe --format bintsv4 --graph ${GRAPH} --prefix "${OUT_PREFIX}-${MOAB_JOBID}" --ncpus 16
