a83e0d18d99aa0f24b332c6eb10a1bfd12646606
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-01-10 13:44:05 +0100

    add moab script for webgraph conversion

---
+++ scripts/moab/convert_web_graph.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#MSUB -N convert web graph
+#MSUB -l nodes=1:ppn=1
+#MSUB -l mem=64gb
+#MSUB -l walltime=05:00:00
+#MSUB -q singlenode
+
+echo "${MOAB_JOBNAME} running on ${MOAB_PROCCOUNT} cores with ${MOAB_NODECOUNT} tasks and ${THRILL_WORKERS_PER_HOST} threads"
+
+executable="java -jar $HOME/webgraphconverter-1.0-SNAPSHOT.jar"
+echo $executable "$WORK/eu-2015-hc" "$WORK/eu-2015.graph"
+exec $executable "$WORK/eu-2015-hc" "$WORK/eu-2015.graph"
