370a12430eba551c873b75b8eca269711b4cf395
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-02-01 14:00:41 +0100

    limit real world to stuff we are going to use

--- scripts/experiments/all_real_seq.rb
+++ scripts/experiments/all_real_seq.rb
@@ -3,13 +3,13 @@
 jobs = [
   ["#{ENV['HOME']}/graphs/uk-2002.metis-preprocessed-*.bin", 1, 0],
   ["#{ENV['HOME']}/graphs/uk-2007-05.metis-preprocessed-*.bin", 3, 0],
-  ["#{ENV['HOME']}/graphs/in-2004.metis-preprocessed-*.bin", 1, 0],
+  # ["#{ENV['HOME']}/graphs/in-2004.metis-preprocessed-*.bin", 1, 0],
   ["#{ENV['HOME']}/graphs/com-friendster-preprocessed-*.bin", 1, 0],
   ["#{ENV['HOME']}/graphs/com-lj.ungraph-preprocessed-*.bin", 1, 0],
   ["#{ENV['HOME']}/graphs/com-orkut.ungraph-preprocessed-*.bin", 1, 0],
-  ["#{ENV['HOME']}/graphs/com-youtube.ungraph-preprocessed-*.bin", 0, 10],
-  ["#{ENV['HOME']}/graphs/com-amazon.ungraph-preprocessed-*.bin", 0, 10],
-  ["#{ENV['HOME']}/graphs/europe.osm-preprocessed-*.bin", 1, 0],
+  # ["#{ENV['HOME']}/graphs/com-youtube.ungraph-preprocessed-*.bin", 0, 10],
+  # ["#{ENV['HOME']}/graphs/com-amazon.ungraph-preprocessed-*.bin", 0, 10],
+  # ["#{ENV['HOME']}/graphs/europe.osm-preprocessed-*.bin", 1, 0],
 ]
 
 `mkdir clusterings`
